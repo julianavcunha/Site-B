2cd9fdbc0f60891ab8cfe4de99313675dc91cd1d
julianavcunha <84291340+julianavcunha@users.noreply.github.com>
2026-06-22 17:11:24 -0300

    Some style changes!

--- site/Class.js
+++ site/Class.js
@@ -1,42 +1,40 @@
-
 class Roupas {
-    constructor (tipo, tamanho, preço, marca, cor) {
+    constructor(tipo, tamanho, preço, marca, cor) {
         this._tipo = tipo;
         this._tamanho = tamanho;
         this._preço = preço;
         this._marca = marca;
         this._cor = cor;
     }
-
 }
 
 class Vestidos extends Roupas {
-    constructor (comprimento) {
+    constructor(tipo, tamanho, preço, marca, cor, comprimento) {
         super(tipo, tamanho, preço, marca, cor);
-        this._comprimento = comprimento; 
+        this._comprimento = comprimento;
     }
 }
 
 class Calças extends Roupas {
-    constructor() {
+    constructor(tipo, tamanho, preço, marca, cor) {
         super(tipo, tamanho, preço, marca, cor);
     }
 }
 
 class Blusas extends Roupas {
-    constructor() {
+    constructor(tipo, tamanho, preço, marca, cor) {
         super(tipo, tamanho, preço, marca, cor);
     }
 }
 
 class Casacos extends Roupas {
-    constructor() {
+    constructor(tipo, tamanho, preço, marca, cor) {
         super(tipo, tamanho, preço, marca, cor);
     }
 }
 
 class Jaquetas extends Roupas {
-    constructor() {
-        super(tipo, tamanho, preço, marca, cor); 
+    constructor(tipo, tamanho, preço, marca, cor) {
+        super(tipo, tamanho, preço, marca, cor);
     }
 }
